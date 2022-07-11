a36424f074c9c19cbfb38ca057153e3a60c6b1ed
Priyam Raj <hello@priyamraj.com>
2022-07-12 00:10:54 +0530

    Optimized Code

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
+document.getElementById("downloadButtons").style.display = "none";
+
+$("#fetchButton").click(function () {
+  $("#downloadButtons").fadeIn(250);
+  $("#fetchButton").text("Fetch Again");
+});
+
 function fetchThumbnail(videoURL) {
   let finalVideoURL =
     /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/;
+
+  const thumbnailURL = "http://img.youtube.com/vi/";
   let match = videoURL.match(finalVideoURL);
 
   if (match && match[1].length == 11) {
     let vidurl = match[1];
-
-    thumbnailpreview = "http://img.youtube.com/vi/" + vidurl + "/mqdefault.jpg";
-    maxres = "http://img.youtube.com/vi/" + vidurl + "/maxresdefault.jpg";
-    midres = "http://img.youtube.com/vi/" + vidurl + "/hqdefault.jpg";
-    lowres = "http://img.youtube.com/vi/" + vidurl + "/mqdefault.jpg";
-  } else {
+    thumbnailpreview = thumbnailURL + vidurl + "/mqdefault.jpg";
+    maxres = thumbnailURL + vidurl + "/maxresdefault.jpg";
+    midres = thumbnailURL + vidurl + "/hqdefault.jpg";
+    lowres = thumbnailURL + vidurl + "/mqdefault.jpg";
+  } 
+  
+  else {
     alert("Please enter a correct 'YouTube video' URL");
     location.reload();
   }
@@ -20,10 +30,3 @@ function fetchThumbnail(videoURL) {
   document.getElementById("midres").href = midres;
   document.getElementById("lowres").href = lowres;
 }
-
-$("#fetchButton").click(function () {
-  $("#downloadButtons").fadeIn(250);
-  $("#fetchButton").text("Fetch Again");
-});
-
-document.getElementById("downloadButtons").style.display = "none";
